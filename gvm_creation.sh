9cac8c67f2960d2ea89b3cf3e6dec2aae2521866
akhil.tejas <[AkhilTejas225@outlook.com]>
2025-05-13 19:06:31 -0500

    added changes

---
+++ gvm_creation.sh
@@ -0,0 +1,7 @@
+gcloud compute instances create mygcp2 \
+           --zone=us-central1-a \
+           --image-family=ubuntu-2204-lts \
+           --image-project=ubuntu-os-cloud \
+           --machine-type=e2-micro \
+           --tags=openall \
+
