7385f4dbd23089d718ba1f0d18e11c5772f8627c
akhil.tejas <[AkhilTejas225@outlook.com]>
2025-05-16 21:00:35 -0500

    added changes

--- gvm_creation_ubuntu.sh
+++ gvm_creation_ubuntu.sh
@@ -1,7 +1,8 @@
-gcloud compute instances create mygcp1 mygcp2 \
+gcloud compute instances create  \
            --zone=us-central1-a \
            --image-family=ubuntu-2204-lts \
            --image-project=ubuntu-os-cloud \
-           --machine-type=e2-micro \
+           --machine-type=e2-medium \
+           --boot-disk-size=20GB \
            --tags=openall \
 
